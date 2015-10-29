46882220bab1f03812fb8ec3a8e25ec880711799
valentinsvt <valentinsvt@hotmail.com>
2015-10-29 10:56:14 -0500

    inicial

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,18 @@
+import org.codehaus.groovy.grails.commons.ApplicationAttributes
+
 class BootStrap {
 
     def init = { servletContext ->
+        def ctx=servletContext.getAttribute(
+                ApplicationAttributes.APPLICATION_CONTEXT)
+        def dataSource = ctx.dataSourceUnproxied
+        dataSource.setMinEvictableIdleTimeMillis(1000 * 60 * 10)
+        dataSource.setTimeBetweenEvictionRunsMillis(1000 * 60 * 10)
+        dataSource.setNumTestsPerEvictionRun(3)
+        dataSource.setTestOnBorrow(true)
+        dataSource.setTestWhileIdle(false)
+        dataSource.setTestOnReturn(true)
+        dataSource.setValidationQuery("SELECT 1")
     }
     def destroy = {
     }
--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -33,7 +33,9 @@ environments {
     production {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            url = "jdbc:mysql://190.107.66.50/esicc?useUnicode=yes&characterEncoding=UTF-8"
+            username = "root"
+            password = "eldia2k"
             properties {
                // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
                jmxEnabled = true
